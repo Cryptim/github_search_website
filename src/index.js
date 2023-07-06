7947968615df0b46674992d44c3e661a764ef6ff
cryptim <timothyignatiusa@gmail.com>
2023-07-06 05:44:54 +0100

    almost done with authentication

--- src/index.js
+++ src/index.js
@@ -5,12 +5,23 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { GithubProvider } from './context/context';
 import { Auth0Provider } from '@auth0/auth0-react';
+//DOMAIN AND CLEIENT ID 
+//dev-oxuc8ze840nn8d30.us.auth0.com
+//XdHsJtryEL1cg4hFU1jQOJyO0FMtFGgG,
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Auth0Provider
+    domain="dev-oxuc8ze840nn8d30.us.auth0.com"
+    clientId="XdHsJtryEL1cg4hFU1jQOJyO0FMtFGgG"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+    >
     <GithubProvider>
     <App />
     </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>
 );
 
